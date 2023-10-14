ea3971e7cd408cbfc5d3632d52ef42525eb0fad2
Totom94 <tom.caldevilla--gonzalvez@efrei.net>
2023-10-14 12:41:50 +0200

    test

--- proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
+++ proj1/src/main/java/agh/ii/prinjava/proj1/MyStack.java
@@ -2,17 +2,23 @@ package agh.ii.prinjava.proj1;
 
 import agh.ii.prinjava.proj1.impl.MyStackDLLBImpl;
 
+/** Interface for creating a stack */
 public interface MyStack<E> {
+    /** Remove the item at the top of the stack and turn it over */
     E pop();
 
+    /** Put an element of type E at the top of the stack */
     void push(E x);
 
+    /** Check whether the stack is empty or not, if so: return true, and vice versa */
     default boolean isEmpty() {
         return numOfElems() == 0;
     }
 
+    /** Return the number of element of the stack */
     int numOfElems();
 
+    /** Returns the top element of the stack without removing it */
     E peek();
 
     /** Consider pros and cons of having a factory method in the interface */
